cedaa4c488b9ee2eaaa3beb124033846bcc97560
MUNEEB AHMED <muneebpandith@gmail.com>
2019-10-25 10:59:05 +0530

    Update MultinomialLogisticRegression.py

--- Multinomial Logistic Regression using Libraries/MultinomialLogisticRegression.py
+++ Multinomial Logistic Regression using Libraries/MultinomialLogisticRegression.py
@@ -1,26 +1,36 @@
 from sklearn import datasets, linear_model, metrics 
+import matplotlib.pyplot as plt 
+
+def MuneebsMain():
+   # load the MNIST like digit dataset 
+   digits = datasets.load_digits() 
    
-# load the digit dataset 
-digits = datasets.load_digits() 
-   
-# defining feature matrix(X) and response vector(y) 
-X = digits.data 
-y = digits.target 
+   # defining feature matrix(X) and response vector(y) 
+   X = digits.data 
+   y = digits.target 
   
-# splitting X and y into training and testing sets 
-from sklearn.model_selection import train_test_split 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, 
+   #Lets display some digits
+   plt.gray() 
+   plt.matshow(digits.images[0]) 
+   plt.show()    
+
+   # splitting X and y into training and testing sets 
+   from sklearn.model_selection import train_test_split 
+   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, 
                                                     random_state=1) 
    
-# create logistic regression MODEL
-reg = linear_model.LogisticRegression() 
+   # create logistic regression MODEL
+   reg = linear_model.LogisticRegression() 
    
-# train the model using the training sets 
-reg.fit(X_train, y_train) 
+   # train the model using the training sets 
+   reg.fit(X_train, y_train) 
   
-# making predictions on the testing set 
-y_pred = reg.predict(X_test) 
+   # making predictions on the testing set 
+   y_pred = reg.predict(X_test) 
    
-# comparing actual response values (y_test) with predicted response values (y_pred) 
-print("Logistic Regression model accuracy(in %):",  
-metrics.accuracy_score(y_test, y_pred)*100) 
+   # comparing actual response values (y_test) with predicted response values (y_pred) 
+   print("Logistic Regression model accuracy(in %):",  
+   metrics.accuracy_score(y_test, y_pred)*100) 
+
+if __name__== "__main__":
+   MuneebsMain()
