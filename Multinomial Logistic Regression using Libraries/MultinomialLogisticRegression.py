51ad70f451f8cbf31e5ff345056bc01295f13b99
MUNEEB AHMED <muneebpandith@gmail.com>
2019-10-25 10:55:52 +0530

    Update MultinomialLogisticRegression.py

--- Multinomial Logistic Regression using Libraries/MultinomialLogisticRegression.py
+++ Multinomial Logistic Regression using Libraries/MultinomialLogisticRegression.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, 
                                                     random_state=1) 
    
-# create logistic regression object 
+# create logistic regression MODEL
 reg = linear_model.LogisticRegression() 
    
 # train the model using the training sets 
